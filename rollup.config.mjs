67f6ef2ca8cb9d9cb9ead288d745b09e83f3297f
Jake Johnston <jjohnston@gfxlabs.io>
2023-12-08 12:10:33 -0800

    start

---
+++ rollup.config.mjs
@@ -0,0 +1,64 @@
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import rollupJson from '@rollup/plugin-json';
+import nodeGlobals from 'rollup-plugin-node-globals'
+import commonjs from '@rollup/plugin-commonjs';
+
+
+const browserConfig = {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/browser.js',
+      format: 'umd',
+      name: "oku-chains",
+    },
+    {
+      file: 'dist/index.mjs',
+      format: 'es',
+    },
+    {
+      file: 'dist/index-mjs.js',
+      format: 'es',
+    }
+  ],
+  plugins: [
+    typescript(),
+    nodeResolve({
+      browser: true,
+      jsnext: true,
+      preferBuiltins: true,
+    }),
+    rollupJson({ compact: true }),
+    nodeGlobals(),
+  ]
+}
+
+const nodeConfig = {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      name: "oku-chains",
+    }
+  ],
+  external: ["ws", "isomorphic-ws"],
+  plugins: [
+    typescript(),
+    nodeResolve({
+      browser: false,
+      jsnext: true,
+      preferBuiltins: true,
+    }),
+    rollupJson({ compact: true }),
+    nodeGlobals(),
+    commonjs({}),
+  ]
+}
+
+
+export default [
+  browserConfig,
+  nodeConfig,
+];
--- src/chains/index.ts
+++ src/chains/index.ts
@@ -1,3 +1,5 @@
+import { Chain, ChainContract } from 'viem'
+
 export { optimism } from './definitions/optimism.js'
 export { arbitrum } from './definitions/arbitrum.js'
 export { bsc } from './definitions/bsc.js'
@@ -8,3 +10,25 @@ export { zkSync } from './definitions/zksync.js'
 export { polygon } from './definitions/polygon.js'
 export { mainnet } from './definitions/mainnet.js'
 export { rootstock } from './definitions/rootstock.js'
+
+export interface IChainInfo extends Chain {
+  logoUrl: string
+  defaultPool: string
+  internalName: string
+  defaultToken0: string
+  defaultToken1: string
+  tokenList: { symbol: string; address: string }[]
+  stables: string[]
+  watchlist: string[]
+  blockTimeSeconds: number
+  contracts: {
+    [key: string]: ChainContract | { [chainId: number]: ChainContract }
+  } & {
+    ensRegistry?: ChainContract
+    ensUniversalResolver?: ChainContract
+    multicall3?: ChainContract
+    nftManager: ChainContract
+    limitOrder?: ChainContract
+    weth9: ChainContract
+  }
+}
