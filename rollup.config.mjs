4c70ada87e9b903cc0173e4421634101b7b3ac72
Jake Johnston <jjohnston@gfxlabs.io>
2023-12-11 14:23:28 -0800

    changes

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,32 @@
 import typescript from '@rollup/plugin-typescript';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 import rollupJson from '@rollup/plugin-json';
 import nodeGlobals from 'rollup-plugin-node-globals'
 import commonjs from '@rollup/plugin-commonjs';
+
+
+const browserConfig = {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/browser.js',
+      format: 'umd',
+      name: "jsrpc",
+    },
+    {
+      file: 'dist/index.mjs',
+      format: 'es',
+    },
+    {
+      file: 'dist/index-mjs.js',
+      format: 'es',
+    }
+  ],
+  plugins: [
+    typescript(),
+    rollupJson({ compact: true }),
+    nodeGlobals(),
+  ]
+}
 
 const nodeConfig = {
   input: 'src/index.ts',
@@ -10,22 +34,20 @@ const nodeConfig = {
     {
       file: 'dist/index.js',
       format: 'cjs',
-      name: "oku-chains",
+      name: "jsrpc",
     }
   ],
+  external: ["ws", "isomorphic-ws"],
   plugins: [
     typescript(),
-    nodeResolve({
-      browser: false,
-      jsnext: true,
-      preferBuiltins: true,
-    }),
     rollupJson({ compact: true }),
     nodeGlobals(),
     commonjs({}),
   ]
 }
 
+
 export default [
+  browserConfig,
   nodeConfig,
 ];
