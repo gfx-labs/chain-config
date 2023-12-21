dcaf5131f4655b517006042726f1aa66ac310c6e
Jake Johnston <jjohnston@gfxlabs.io>
2023-12-21 09:51:08 -0800

    zkevm default

--- src/definitions/zkevm.ts
+++ src/definitions/zkevm.ts
@@ -1,14 +1,13 @@
 import { polygonZkEvm as chain } from 'viem/chains'
 import { IChainInfo } from '..'
-import { zeroAddress } from 'viem'
 
 export const polygonZkEvm: IChainInfo = {
   ...chain,
   blockTimeSeconds: 7,
   logoUrl: 'https://assets.oku.trade/zkevm-logo.svg',
-  defaultPool: zeroAddress,
-  defaultToken0: zeroAddress,
-  defaultToken1: zeroAddress,
+  defaultPool: '0xd2c7e323c6f5415dc0ee4e3811901b3380d316fd',
+  defaultToken0: '0x4f9a0e7fd2bf6067db6994cf12e4495df938e6e9',
+  defaultToken1: '0xa8ce8aee21bc2a48a5ef670afcc9274c7bbbc035',
   tokenList: [
     { symbol: 'WETH', address: '0x4f9a0e7fd2bf6067db6994cf12e4495df938e6e9' },
     { symbol: 'USDC', address: '0xa8ce8aee21bc2a48a5ef670afcc9274c7bbbc035' },
@@ -16,7 +15,7 @@ export const polygonZkEvm: IChainInfo = {
     { symbol: 'DAI', address: '0xc5015b9d9161dca7e18e32f6f25c4ad850731fd4' }
   ],
   stables: ['0x1e4a5963abfd975d8c9021ce480b42188849d41d', '0xa8ce8aee21bc2a48a5ef670afcc9274c7bbbc035', '0xc5015b9d9161dca7e18e32f6f25c4ad850731fd4'],
-  watchlist: [zeroAddress],
+  watchlist: ['0xd2c7e323c6f5415dc0ee4e3811901b3380d316fd'],
   internalName: 'polygon-zkevm',
   contracts: {
     ...chain.contracts,
