706ac010c3f50081d7593a5c9ebf0705c63132fe
a <a@tuxpa.in>
2024-05-28 15:52:03 -0500

    fix: anyany

--- src/spec/index.ts
+++ src/spec/index.ts
@@ -44,7 +44,7 @@ export interface TokenMetadata {
   usdcAddress?: Address;
 }
 
-export interface IChainInfo extends Chain {
+export interface IChainInfo extends Chain<any, any> {
   logoUrl: string;
   sortIndex: number;
   safeReorgDistance?: number;
