2cf07d0111c354addb2ca4102ad407bb30525893
a <a@tuxpa.in>
2024-05-28 16:07:11 -0500

    fix: build

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,6 +1,7 @@
+import { ChainFormatters } from "viem";
 import { IChainInfo } from "../spec";
 
-export const makeConfig = <T extends IChainInfo>(
+export const makeConfig = <T extends IChainInfo<ChainFormatters>>(
   x: Readonly<T>,
 ): Readonly<T> => {
   return x;
