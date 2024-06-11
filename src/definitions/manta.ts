73fb99b18a6d79629874f1c823a9a65fa905e9a5
Jake Johnston <jjohnston@gfxlabs.io>
2024-06-11 13:53:01 -0700

    fix: manta native

--- src/definitions/manta.ts
+++ src/definitions/manta.ts
@@ -61,7 +61,7 @@ export const manta = makeConfig({
     "0x5bedc5d1e1d0b32caca78db23af71126b6fe26b6",
   ],
   internalName: "manta",
-  nativeLogoUrl: "https://assets.oku.trade/natives/manta.png",
+  nativeLogoUrl: "https://assets.oku.trade/natives/eth.png",
   contracts: {
     ...chain.contracts,
     limitOrder: {
