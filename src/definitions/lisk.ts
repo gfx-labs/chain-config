dd6dae132634683ce5fffd6d0a9795558910f550
Jake Johnston <jjohnston@gfxlabs.io>
2024-07-30 14:17:23 -0700

    fix: lisk

--- src/definitions/lisk.ts
+++ src/definitions/lisk.ts
@@ -6,7 +6,7 @@ export const lisk = makeConfig({
   ...chain,
   blockTimeSeconds: 2,
   sortIndex: 15,
-  logoUrl: "https://assets.oku.trade/lisk-logo.svg",
+  logoUrl: "https://assets.oku.trade/lisk-logo.png",
   safeReorgDistance: 90000,
   externalId: {
     zerion: "lisk",
