bb61bc3326ebafe3b85d100e478c0186c136c141
Jake Johnston <jjohnston@gfxlabs.io>
2024-08-13 09:13:15 -0700

    fix: nevermind

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,31 +1,8 @@
-import { ChainFormatters, getAddress, isAddress } from "viem";
+import { ChainFormatters } from "viem";
 import { IChainInfo } from "../spec";
 
 export const makeConfig = <T extends IChainInfo<ChainFormatters>>(
-  x: T,
+  x: Readonly<T>,
 ): Readonly<T> => {
-  const checksumAddresses = (input: any): any => {
-    if (Array.isArray(input)) {
-      return input.map(checksumAddresses);
-    }
-
-    if (typeof input === "object" && input !== null) {
-      return Object.keys(input).reduce((result, key) => {
-        const value = input[key];
-        result[key] = isStringAndAddress(value)
-          ? getAddress(value)
-          : checksumAddresses(value);
-        return result;
-      }, {} as any);
-    }
-    return input;
-  };
-
-  const isStringAndAddress = (value: any): boolean => {
-    return typeof value === "string" && isAddress(value, { strict: false });
-  };
-
-  const checksummedConfig = checksumAddresses(x);
-
-  return Object.freeze(checksummedConfig);
+  return x;
 };
