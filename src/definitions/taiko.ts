1c9abb64c0b13a5429d85575ea81f5928eed617d
Jake Johnston <jjohnston@gfxlabs.io>
2024-08-15 08:19:31 -0700

    fix: taiko stables

--- src/definitions/taiko.ts
+++ src/definitions/taiko.ts
@@ -25,14 +25,14 @@ export const taiko = makeConfig({
           id: 2,
           address: "0x1C10CC06DC6D35970d1D53B2A23c76ef370d4135",
         },
-      ]
-    }
+      ],
+    },
   },
   oracles: {
     coingecko: {
       slug: "taiko",
-      native: "ethereum"
-    }
+      native: "ethereum",
+    },
   },
   blockExplorers: {
     default: {
@@ -43,7 +43,8 @@ export const taiko = makeConfig({
     routescan: {
       name: "Routescan",
       url: "https://taikoscan.network",
-      apiUrl: "https://api.routescan.io/v2/network/mainnet/evm/43114/etherscan/api",
+      apiUrl:
+        "https://api.routescan.io/v2/network/mainnet/evm/43114/etherscan/api",
     },
   },
   uniswap: {
@@ -77,7 +78,11 @@ export const taiko = makeConfig({
     { symbol: "WETH", address: "0xA51894664A773981C6C112C43ce576f315d5b1B6" },
     { symbol: "USDC", address: "0x07d83526730c7438048D55A4fc0b850e2aaB6f0b" },
   ],
-  stables: ["0x07d83526730c7438048D55A4fc0b850e2aaB6f0b"],
+  stables: [
+    "0x07d83526730c7438048D55A4fc0b850e2aaB6f0b",
+    "0x19e26b0638bf63aa9fa4d14c6baf8d52ebe86c5c",
+    "0x9c2dc7377717603eB92b2655c5f2E7997a4945BD",
+  ],
   watchlist: [
     "0xDaC937d4263E6A667A027FE59B2FFe2F91D54f46",
     "0x5B731355AAd31C132dd4754E70E92c590Ae42386",
