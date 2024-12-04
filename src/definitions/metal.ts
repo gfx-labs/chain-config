d93d9c82d24337c8746516f1ae15eaf65b98690e
Jake Johnston <jjohnston@gfxlabs.io>
2024-12-04 15:49:56 -0800

    fix: metal logo

--- src/definitions/metal.ts
+++ src/definitions/metal.ts
@@ -7,7 +7,7 @@ export const metal = makeConfig({
   blockTimeSeconds: 2,
   launchTime: 1733882663,
   sortIndex: 15,
-  logoUrl: "https://assets.oku.trade/metal-logo.svg",
+  logoUrl: "https://assets.oku.trade/metal-logo.png",
   safeReorgDistance: 90000,
   externalId: {},
   markets: {},
