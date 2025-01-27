8e6109094e79d3d9c6ddc2a12a6bd2f48fcdd9dd
Jake Johnston <jjohnston@gfxlabs.io>
2025-01-27 12:37:10 -0800

    fix: hemi initial pool

--- src/definitions/hemi.ts
+++ src/definitions/hemi.ts
@@ -45,7 +45,7 @@ export const hemi = makeConfig({
       nativeWrappedName: "ETH",
     },
   },
-  defaultPool: zeroAddress,
+  defaultPool: "0x3A49cCD3c2A688D0766E087B30A3d27C8997A71c",
   defaultToken0: "0x4200000000000000000000000000000000000006",
   defaultToken1: "0xad11a8BEb98bbf61dbb1aa0F6d6F2ECD87b35afA",
   tokenList: [
@@ -59,7 +59,7 @@ export const hemi = makeConfig({
     "0xbB0D083fb1be0A9f6157ec484b6C79E0A4e31C2e",
     "0x6c851F501a3F24E29A8E39a29591cddf09369080",
   ],
-  watchlist: [],
+  watchlist: ["0x3A49cCD3c2A688D0766E087B30A3d27C8997A71c"],
   internalName: "hemi",
   nativeLogoUrl: "https://assets.oku.trade/natives/eth.png",
   contracts: {
