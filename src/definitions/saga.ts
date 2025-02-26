acb0727f3c8eb3cd5ab4637c56a2763bdc55a6bf
Jake Johnston <jjohnston@gfxlabs.io>
2025-02-26 09:30:19 -0800

    fix: saga urls

--- src/definitions/saga.ts
+++ src/definitions/saga.ts
@@ -14,6 +14,18 @@ export const saga = makeConfig({
   bridges: {},
   oracles: {},
   morpho: {},
+  blockExplorers: {
+    default: {
+      name: "BlockScout",
+      url: "https://sagaevm.sagaexplorer.io",
+    },
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://sagaevm.jsonrpc.sagarpc.io"],
+      webSocket: ["wss://sagaevm.ws.sagarpc.io"],
+    },
+  },
   initCodeHash:
     "0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54",
   uniswap: {
