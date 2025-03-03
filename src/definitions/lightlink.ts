80a0409aa6dfc0825b3d9c8f5ec37a1a3f37bbd9
Jake Johnston <jjohnston@gfxlabs.io>
2025-03-03 14:53:28 -0800

    fix: redo lightlink

--- src/definitions/lightlink.ts
+++ src/definitions/lightlink.ts
@@ -1,5 +1,6 @@
 import { lightlinkPhoenix as chain } from "viem/chains";
 import { makeConfig } from "../util";
+import { zeroAddress } from "viem";
 
 export const lightlink = makeConfig({
   ...chain,
@@ -35,7 +36,7 @@ export const lightlink = makeConfig({
   },
   token: {
     wethAddress: "0x7ebef2a4b1b09381ec5b9df8c5c6f2dbeca59c73",
-    usdcAddress: "0x18fB38404DADeE1727Be4b805c5b242B5413Fa40",
+    usdcAddress: "0xbCF8C1B03bBDDA88D579330BDF236B58F8bb2cFd",
     wbtcAddress: "0x46A5e3Fa4a02B9Ae43D9dF9408C86eD643144A67",
   },
   oku: {
@@ -46,20 +47,21 @@ export const lightlink = makeConfig({
       nativeWrappedName: "WETH",
     },
   },
-  defaultPool: "0x5B95152D1d8F9Ce6a3aAEACE0514879eD24B768e",
-  defaultToken0: "0x18fB38404DADeE1727Be4b805c5b242B5413Fa40",
-  defaultToken1: "0x7ebef2a4b1b09381ec5b9df8c5c6f2dbeca59c73",
+  defaultPool: zeroAddress,
+  defaultToken0: "0x7ebef2a4b1b09381ec5b9df8c5c6f2dbeca59c73",
+  defaultToken1: "0xbCF8C1B03bBDDA88D579330BDF236B58F8bb2cFd",
   tokenList: [
     { symbol: "LL", address: "0xd9d7123552fA2bEdB2348bB562576D67f6E8e96E" },
-    { symbol: "USDC.e", address: "0x18fB38404DADeE1727Be4b805c5b242B5413Fa40" },
+    { symbol: "USDC.e", address: "0xbCF8C1B03bBDDA88D579330BDF236B58F8bb2cFd" },
+    { symbol: "USDT", address: "0x808d7c71ad2ba3FA531b068a2417C63106BC0949" },
     { symbol: "WETH", address: "0x7ebef2a4b1b09381ec5b9df8c5c6f2dbeca59c73" },
     { symbol: "WBTC", address: "0x46A5e3Fa4a02B9Ae43D9dF9408C86eD643144A67" },
   ],
   stables: [
-    "0x18fB38404DADeE1727Be4b805c5b242B5413Fa40",
-    "0x6308fa9545126237158778e74AE1b6b89022C5c0",
+    "0xbCF8C1B03bBDDA88D579330BDF236B58F8bb2cFd",
+    "0x808d7c71ad2ba3FA531b068a2417C63106BC0949",
   ],
-  watchlist: ["0x5B95152D1d8F9Ce6a3aAEACE0514879eD24B768e"],
+  watchlist: [],
   internalName: "lightlink",
   nativeLogoUrl: "https://assets.oku.trade/natives/eth.png",
   contracts: {
