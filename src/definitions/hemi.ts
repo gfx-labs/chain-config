01f5c3b1daa320fc4ef2a8d0aa55f77dbfe66314
Jake Johnston <jjohnston@gfxlabs.io>
2025-03-13 14:33:29 -0700

    fix: hemi launch time

--- src/definitions/hemi.ts
+++ src/definitions/hemi.ts
@@ -4,7 +4,7 @@ import { makeConfig } from "../util";
 export const hemi = makeConfig({
   ...chain,
   blockTimeSeconds: 12,
-  launchTime: 1738569600,
+  launchTime: 1741901593,
   sortIndex: 13,
   logoUrl: "https://assets.oku.trade/chains/hemi-logo.svg",
   safeReorgDistance: 90000,
