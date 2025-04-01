b682a152351bdbc668c7b34fc6fe8e6c30d2aab1
Jake Johnston <jjohnston@gfxlabs.io>
2025-04-01 13:55:12 -0700

    fix: corn watchlist and token

--- src/definitions/corn.ts
+++ src/definitions/corn.ts
@@ -65,8 +65,12 @@ export const corn = makeConfig({
     { symbol: "WBTCN", address: "0xda5dDd7270381A7C2717aD10D1c0ecB19e3CDFb2" },
     { symbol: "USDC.e", address: "0xDF0B24095e15044538866576754F3C964e902Ee6" },
   ],
-  stables: ["0xDF0B24095e15044538866576754F3C964e902Ee6"],
+  stables: [
+    "0xDF0B24095e15044538866576754F3C964e902Ee6",
+    "0xB8CE59FC3717ada4C02eaDF9682A9e934F625ebb",
+  ],
   watchlist: [
+    "0x660c6c6c2ad9be9fca2d40da22e1e6142ce5e7ca",
     "0xe435E14815d28a86c643E4606Ed0e8E5624782c6",
     "0x43aA32cf4f6389aB89d8C79092A0fc65899C20ba",
     "0x0F12dac5217868b3d6772632543ef16df8df17E5",
