9c1f829da68fd5c91e3cac7e95605dc561ad4583
Jake Johnston <jjohnston@gfxlabs.io>
2025-04-03 21:29:58 -0700

    fix: telostx

--- src/definitions/telos.ts
+++ src/definitions/telos.ts
@@ -13,6 +13,12 @@ export const telos = makeConfig({
     openocean: "telos",
     icecreamswap: true,
   },
+  blockExplorers: {
+    default: {
+      name: "Blockscout",
+      url: "https://telostx.com",
+    },
+  },
   bridges: {
     stargate: {
       endpointID: 30199,
