9cf264444333a1b585184c66d4fe6858e32a203b
Jake Johnston <jjohnston@gfxlabs.io>
2025-07-30 08:34:07 -0700

    fix: icecreamswap on hemi

--- src/definitions/hemi.ts
+++ src/definitions/hemi.ts
@@ -10,7 +10,9 @@ export const hemi = makeConfig({
 	safeReorgDistance: 90000,
 	blockAid: "",
 	externalId: {},
-	markets: {},
+	markets: {
+		icecreamswap: true,
+	},
 	bridges: {
 		stargate: {
 			endpointID: 30329,
