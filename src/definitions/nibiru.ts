53d37d932d87e4534fcf843c86936ff5faaec5d4
Jake Johnston <jjohnston@gfxlabs.io>
2025-09-12 08:35:03 -0700

    fix: plasma usdt

--- src/definitions/nibiru.ts
+++ src/definitions/nibiru.ts
@@ -85,6 +85,8 @@ export const nibiru = makeConfig({
 		"0x0085a09e68468a99c6ec3c0c9c98e17af614c470",
 		"0x9fe58f1883732931625c281afe2a068fe19183f5",
 		"0xd8f82b8d2e82265ac25d8d4ef3ca47917693d3d5",
+		"0x5133222db1e9f1b98d16e5fb46d6f398c60998e2",
+		"0xe7612e14a05613caf06fad766b484b0a6d146c3a",
 	],
 	internalName: "nibiru",
 	nativeLogoUrl: "https://assets.oku.trade/natives/nibi.svg",
