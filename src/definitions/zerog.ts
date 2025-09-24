0c77ac612d4aa0d71160ca8d36791687ec4e3e21
Jake Johnston <jjohnston@gfxlabs.io>
2025-09-24 13:34:17 -0700

    fix: 0g initial pool

--- src/definitions/zerog.ts
+++ src/definitions/zerog.ts
@@ -1,4 +1,3 @@
-import { zeroAddress } from "viem";
 import { zeroGMainnet as chain } from "viem/chains";
 import { makeConfig } from "../util";
 
@@ -50,7 +49,7 @@ export const zerog = makeConfig({
 			nativeWrappedName: "0G",
 		},
 	},
-	defaultPool: zeroAddress,
+	defaultPool: "0x5c1e4669c5208adc0163273d51928890b663237f",
 	defaultToken0: "0x1cd0690ff9a693f5ef2dd976660a8dafc81a109c",
 	defaultToken1: "0x1f3aa82227281ca364bfb3d253b0f1af1da6473e",
 	tokenList: [
@@ -58,7 +57,7 @@ export const zerog = makeConfig({
 		{ symbol: "w0G", address: "0x1cd0690ff9a693f5ef2dd976660a8dafc81a109c" },
 	],
 	stables: ["0x1f3aa82227281ca364bfb3d253b0f1af1da6473e"],
-	watchlist: [],
+	watchlist: ["0x5c1e4669c5208adc0163273d51928890b663237f"],
 	internalName: "zerog",
 	nativeLogoUrl: "https://cms.oku.trade/cdn/public/natives/0g.svg",
 	contracts: {
