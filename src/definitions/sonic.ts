6bc5db914e00a32aa8db93d63471919fdcc4219f
Jake Johnston <jjohnston@gfxlabs.io>
2025-12-02 11:44:07 -0800

    fix: sonic

--- src/definitions/sonic.ts
+++ src/definitions/sonic.ts
@@ -8,7 +8,7 @@ export const sonic = makeConfig({
 	transactionType: "eip1559",
 	sortIndex: 15,
 	logoUrl: "https://assets.oku.trade/chains/sonic-logo.svg",
-	deprecated: true,
+	deprecated: false,
 	safeReorgDistance: 90000,
 	blockAid: "",
 	morpho: {},
