250e7de3ec74eb4e1286c3b951594bcbac42d0a0
Jake Johnston <jjohnston@gfxlabs.io>
2025-12-29 14:01:42 -0800

    fix: launch time

--- src/definitions/hyperevem.ts
+++ src/definitions/hyperevem.ts
@@ -6,7 +6,7 @@ export const hyperevm = makeConfig({
 	...chain,
 	name: "HyperEvm",
 	blockTimeSeconds: 1,
-	launchTime: 1739731200,
+	launchTime: 1767333673,
 	transactionType: "eip1559",
 	sortIndex: 15,
 	logoUrl: "https://cms.oku.trade/cdn/public/chains/hyperevm-logo.svg",
