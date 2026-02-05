11f533298bb4cb16b17ca0dad4cf3834e58f4e88
Jake Barber <jbarber@gfxlabs.io>
2026-02-05 18:05:51 +0000

    OkuRouter prod deploys

--- src/definitions/optimism.ts
+++ src/definitions/optimism.ts
@@ -106,7 +106,7 @@ export const optimism = makeConfig({
 		dexscreener: "optimism",
 	},
 	oku: {
-		router: "0x80dCD2C737cAFE9f86559bBCed9938eFfB7f7D1A",
+		router: "0x822CFA9749d16Fb4B4F2B0515924cec69512893b",
 		limitOrderRegistryDeployBlock: 105276132,
 		limitOrderRegistry: "0x54dF9e11c7933a9cA3BD1E540B63dA15edAe40bf",
 		customOrderTypes: {
