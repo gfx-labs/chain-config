fb50df782d3070fb45e452a5ea7a69a08af181d5
a <eddy@gfxlabs.io>
2026-09-10 13:58:00 -0500

    feat: add bitget market router addresses

--- dist/networks/hyperevm.go
+++ dist/networks/hyperevm.go
@@ -56,6 +56,7 @@ var Hyperevm Network = Network{
 
 	Morpho: MorphoMetadata{},
 	MarketRouters: MarketRouters{
+		Bitget:       []common.Address{common.HexToAddress("0x9515d72BcE20cD9e695EE61D2a76A099EA511311")},
 		Enso:         []common.Address{common.HexToAddress("0xF75584eF6673aD213a685a1B58Cc0330B8eA22Cf")},
 		Icecreamswap: []common.Address{common.HexToAddress("0x2fF506ed9729580EF8Bf04429614beB1baE5F76D")},
 		Kyberswap:    []common.Address{common.HexToAddress("0x6131B5fae19EA4f9D964eAc0408E4408b66337b5")},
